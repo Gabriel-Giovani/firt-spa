7a802d17993e901b3da5dc366c59a06860c18946
Gabriel Giovani <gabriel.leite4@fatec.sp.gov.br>
2020-07-21 23:07:29 -0300

    first commit

---
+++ src/components/Billing/Billing.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+class Billing extends Component{
+    
+    constructor(props){
+        
+        super(props);
+
+        this.state = {
+            detalhamento: []
+        }
+
+    }
+
+    componentDidMount(){
+
+        fetch('http://www.devup.com.br/php/api-dashboard/api/faturamento')
+        .then(result => result.json().then(data => this.setState(data)));
+
+    }
+
+    render(){
+        return(
+
+            <div>
+                <h2 className="mt-2">Faturamento</h2>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Total por forma de pagamento
+                            </div>
+
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Descrição</th>
+                                        <th className="text-right">Valor</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.detalhamento.map((item, index) => {
+                                            return(
+
+                                                <tr key={ index }>
+                                                    <td>{item.descricao}</td>
+                                                    <td className="text-right">
+                                                        {item.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
+                                                    </td>
+                                                </tr>
+
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Billing;
---
+++ src/components/Consults/Consults.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+class Consults extends Component{
+    
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            realizadas: [],
+            marcadas: []
+        }
+
+    }
+
+    componentDidMount(){
+
+        fetch('http://www.devup.com.br/php/api-dashboard/api/consultas')
+        .then(result => result.json().then(data => this.setState(data)));
+
+    }
+    
+    render(){
+        return(
+
+            <div>
+                <h2 className="mt-2">Consultas</h2>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="card mt-2">
+                            <div className="card-header">
+                                Realizadas
+                            </div>
+
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Especialidade</th>
+                                        <th>Quantidade</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.realizadas.map((item, index) => {
+                                            return (
+                                                <tr key={ index }>
+                                                    <td>{item.especialidade}</td>
+                                                    <td>{item.quantidade}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="card mt-2">
+                            <div className="card-header">
+                                Marcadas
+                            </div>
+                            
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Especialidade</th>
+                                        <th>Quantidade</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.marcadas.map((item, index) => {
+                                            return(
+
+                                                <tr key={ index }>
+                                                    <td>{item.especialidade}</td>
+                                                    <td>{item.quantidade}</td>
+                                                </tr>
+
+                                            );
+                                        })
+                                    }            
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Consults;
